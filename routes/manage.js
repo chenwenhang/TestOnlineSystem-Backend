67a712c525db74d5bf62aef362c651507f4ee8e8
chenwenhang <cwh736@qq.com>
2019-04-25 23:02:32 +0800

    feat(question-collection): add question-collection function

--- routes/manage.js
+++ routes/manage.js
@@ -3,23 +3,25 @@
  * @Description: 
  * @Github: https://github.com/chenwenhang
  * @Date: 2019-04-12 21:20:51
- * @LastEditTime: 2019-04-13 16:02:19
+ * @LastEditTime: 2019-04-25 21:58:09
  */
-var express=require('express');
-var router = express.Router(); 
-var user=require('./manage/user.js');
-var occupation=require('./manage/occupation.js');
-var paper=require('./manage/paper.js');
-var paper_history=require('./manage/paper_history.js');
-var question=require('./manage/question.js');
-var tag=require('./manage/tag.js');
+var express = require('express');
+var router = express.Router();
+var user = require('./manage/user.js');
+var occupation = require('./manage/occupation.js');
+var paper = require('./manage/paper.js');
+var paper_history = require('./manage/paper_history.js');
+var question = require('./manage/question.js');
+var tag = require('./manage/tag.js');
+var question_collection = require('./manage/question_collection.js');
 
-router.use('/user',user);
-router.use('/occupation',occupation);
-router.use('/paper',paper);
-router.use('/paper_history',paper_history);
-router.use('/question',question);
-router.use('/tag',tag);
-router.use('/user',user);
+router.use('/user', user);
+router.use('/occupation', occupation);
+router.use('/paper', paper);
+router.use('/paper_history', paper_history);
+router.use('/question', question);
+router.use('/tag', tag);
+router.use('/user', user);
+router.use('/question_collection', question_collection);
 
 module.exports = router;
---
+++ routes/manage/question_collection.js
@@ -0,0 +1,121 @@
+/*
+ * @Author: Chen Wenhang
+ * @Description: 
+ * @Github: https://github.com/chenwenhang
+ * @Date: 2019-04-25 18:28:18
+ * @LastEditTime: 2019-04-25 22:59:36
+ */
+var express = require('express');
+var dateFormat = require('dateformat');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var status = require('../../modules/status.js');
+var DB = require('../../modules/db.js');
+
+
+/**
+ * @description: get question_collection
+ * @param {} 
+ * @return: 
+ */
+router.get('/', (req, res) => {
+    if (req.query.content) {
+        DB.find('question_collection', {
+            "content": {
+                $regex: req.query.content
+            },
+            "username": req.query.username
+        }, (err, data, count) => {
+            // console.log(data);
+            if (err) {
+                res.json(status(0, '查询失败'));
+            } else {
+                res.json(status(1, '查询成功', data, count));
+            }
+        }, req.query.page, req.query.size)
+    } else {
+        DB.find('question_collection', {
+            "username": req.query.username
+        }, (err, data, count) => {
+            // console.log(data);
+            if (err) {
+                res.json(status(0, '查询失败'));
+            } else {
+                res.json(status(1, '查询成功', data, count));
+            }
+        }, req.query.page, req.query.size)
+    }
+});
+
+
+/**
+ * @description: add a question to collection
+ * @param {} 
+ * @return: 
+ */
+router.post('/add', (req, res) => {
+    // req.body._id = new DB.ObjectID(req.body._id);
+    DB.insert('question_collection', [req.body], (err, data) => {
+        if (err) {
+            res.json(status(0, '添加失败'));
+        } else {
+            res.json(status(1, '添加成功', data));
+        }
+    })
+});
+
+/**
+ * @description: delete a question from collection
+ * @param {} 
+ * @return: 
+ */
+router.delete('/delete', (req, res) => {
+    DB.delete('question_collection', {
+        "question_id": req.query.question_id,
+        "username": req.query.username
+    }, (err, data) => {
+        if (err) {
+            res.json(status(0, '删除失败'));
+        } else {
+            res.json(status(1, '删除成功', data));
+        }
+    })
+});
+
+/**
+ * @description: delete a question from collection
+ * @param {} 
+ * @return: 
+ */
+router.post('/check', (req, res) => {
+    let collectionId = req.body.collectionId;
+    let username = req.body.username;
+    let collectionNum = [];
+    for (let i = 0; i < collectionId.length; i++) {
+        let question_id = collectionId[i];
+        DB.find('question_collection', {
+            "question_id": question_id,
+            "username": username
+        }, (err, data) => {
+            if (err) {
+                // console.log(err);
+                res.json(status(0, '检验失败'));
+            } else {
+                if (data.length != 0) {
+                    collectionNum.push(1);
+                } else {
+                    collectionNum.push(0);
+                }
+                if (collectionNum.length == collectionId.length) {
+                    // console.log(collectionNum);
+                    res.json(status(1, '检验成功', collectionNum));
+
+                }
+            }
+        })
+    }
+
+
+});
+
+module.exports = router;
